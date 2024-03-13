e630aca066e940b5364b1d28ffeee09eefb0328d
Usama Al Darwashi <143253205+usamadarwashi@users.noreply.github.com>
2024-03-13 15:09:04 +0400

    added a map

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,32 @@
 import './App.css'
-import Usama from "./components/usama.tsx";
-import Table from "./components/table.tsx";
-import {useState} from "react";
-
+// import Usama from "./components/usama.tsx";
+// import Table from "./components/table.tsx";
+// import {useState} from "react";
+import Map from 'react-map-gl';
 function App() {
-  const [count,setCount] = useState(false)
+  // const [count,setCount] = useState(false)
 
-    function toggleTable() {
-        if (count) setCount(false)
-        else setCount(true)
-    }
-
+    // function toggleTable() {
+    //     if (count) setCount(false)
+    //     else setCount(true)
+    // }
     return (
     <>
-        <Usama></Usama>
-        <button onClick={toggleTable}>Toggle Table</button>
-        {
-            count && <Table></Table>
-        }
+        {/*<Usama></Usama>*/}
+        {/*<button onClick={toggleTable}>Toggle Table</button>*/}
+        {/*{*/}
+        {/*    count && <Table></Table>*/}
+        {/*}*/}
+        <Map
+            mapboxAccessToken={"pk.eyJ1IjoidGhhbWlyLWFsaGFydGh5IiwiYSI6ImNsZnRsa3Q4MjAwdXczbnMxaDlycnRvbTAifQ.JyDqUC2S2D2-f0YJUmMxaw"}
+            initialViewState={{
+                longitude: 54.4651,
+                latitude: 21.6160,
+                zoom: 5
+            }}
+            style={{width: '100vw', height: '100vh'}}
+            mapStyle="mapbox://styles/mapbox/streets-v9"
+        />
     </>
   )
 }
