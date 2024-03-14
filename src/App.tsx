ece40fa2c55b20178c57d472d4060f9c679da5a0
Usama Al Darwashi <143253205+usamadarwashi@users.noreply.github.com>
2024-03-14 09:34:27 +0400

    added a widget

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,40 @@
 import './App.css'
-// import Usama from "./components/usama.tsx";
-// import Table from "./components/table.tsx";
-// import {useState} from "react";
-import Map from 'react-map-gl';
+import MapGL from 'react-map-gl';
+
 function App() {
-  // const [count,setCount] = useState(false)
-
-    // function toggleTable() {
-    //     if (count) setCount(false)
-    //     else setCount(true)
-    // }
     return (
-    <>
-        {/*<Usama></Usama>*/}
-        {/*<button onClick={toggleTable}>Toggle Table</button>*/}
-        {/*{*/}
-        {/*    count && <Table></Table>*/}
-        {/*}*/}
-        <Map
-            mapboxAccessToken={"pk.eyJ1IjoidGhhbWlyLWFsaGFydGh5IiwiYSI6ImNsZnRsa3Q4MjAwdXczbnMxaDlycnRvbTAifQ.JyDqUC2S2D2-f0YJUmMxaw"}
-            initialViewState={{
-                longitude: 54.4651,
-                latitude: 21.6160,
-                zoom: 5
-            }}
-            style={{width: '100vw', height: '100vh'}}
-            mapStyle="mapbox://styles/mapbox/streets-v9"
-        />
-    </>
-  )
+        <>
+            <div style={{
+                position: 'absolute',
+                top: 0,
+                left: 0,
+                backgroundColor: 'black',
+                zIndex: 1,
+                padding: '10px',
+                borderRadius: '8px',
+                margin: '10px',
+                maxWidth: '250px',
+                textAlign: 'center',
+            }}>
+                <h1>Widget</h1>
+                <p>Information inside the widget</p>
+            </div>
+            {/*<Usama></Usama>*/}
+            {/*<button onClick={toggleTable}>Toggle Table</button>*/}
+            {/*{*/}
+            {/*    count && <Table></Table>*/}
+            {/*}*/}
+            <MapGL
+                mapboxAccessToken={"pk.eyJ1IjoidGhhbWlyLWFsaGFydGh5IiwiYSI6ImNsZnRsa3Q4MjAwdXczbnMxaDlycnRvbTAifQ.JyDqUC2S2D2-f0YJUmMxaw"}
+                initialViewState={{
+                    longitude: 54.4651,
+                    latitude: 21.6160,
+                    zoom: 5
+                }}
+                style={{width: '100vw', height: '100vh'}}
+                mapStyle="mapbox://styles/mapbox/streets-v9"
+            />
+        </>
+    )
 }
-
-export default App
+export default App;
