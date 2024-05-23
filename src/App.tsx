ff52536728c4b85f485335b79cb4523b7c952358
Usama Al Darwashi <143253205+usamadarwashi@users.noreply.github.com>
2024-05-23 17:46:10 +0400

    gg

--- src/App.tsx
+++ src/App.tsx
@@ -4,27 +4,6 @@ import MapGL from 'react-map-gl';
 function App() {
     return (
         <>
-            <div style={{
-                position: 'absolute',
-                top: 0,
-                left: 0,
-                backgroundColor: 'black',
-                zIndex: 1,
-                padding: '10px',
-                borderRadius: '8px',
-                margin: '10px',
-                maxWidth: '250px',
-                textAlign: 'center',
-            }}>
-                <h1>Oman</h1>
-                <p>سلام عليكوووم</p>
-
-            </div>
-            {/*<Usama></Usama>*/}
-            {/*<button onClick={toggleTable}>Toggle Table</button>*/}
-            {/*{*/}
-            {/*    count && <Table></Table>*/}
-            {/*}*/}
             <MapGL
                 mapboxAccessToken={"pk.eyJ1IjoidGhhbWlyLWFsaGFydGh5IiwiYSI6ImNsZnRsa3Q4MjAwdXczbnMxaDlycnRvbTAifQ.JyDqUC2S2D2-f0YJUmMxaw"}
                 initialViewState={{
--- src/components/usama.tsx
+++
@@ -1,8 +0,0 @@
-
-import '../styles/usama.css'
-
-const usama = () => {
-    return <h1>Dogs Breeds</h1>
-}
-
-export default usama;
